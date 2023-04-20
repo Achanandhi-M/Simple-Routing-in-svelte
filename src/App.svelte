7f7d7500fb468d72a0a83619e049c34a7920b8eb
Achanandhi24 <achanandhi.m@gmail.com>
2023-04-20 15:33:14 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+
+  import {Router ,Route,Link} from "svelte-routing";
+  import Home from "./home.svelte"
+  import About from "./about.svelte";
+    import Projects from "./Projects.svelte";
+</script>
+<Router>
+  <nav>
+   <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+    <Link to="projects">projects</Link>
+  </nav>
+  <div>
+   <Route path="/" component ="{Home}" />
+    <Route path="about" component="{About}" />
+    <Route path="projects" component="{Projects}" />
+  </div>
+</Router>
